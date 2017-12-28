fb4175e7a8df3ab6dcf002d57c5edcc7a2f56b28
stepa <stepa@DESKTOP-G6RT5QA>
2017-12-28 11:43:56 +0400

    ord

---
+++ UnitTestAvto/Avtorizacia.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace TeacoSubscribeNunit
+{
+    public class Avtorizacia
+    {
+        public IWebDriver driver { get; set; }
+        public string text { get; set; }
+
+        public Avtorizacia(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void Test()
+        {
+            TimeSpan timeout = new TimeSpan(00, 01, 00);
+
+            driver.Navigate().GoToUrl("https://eplaza.panasonic.ru/");
+            driver.Manage().Window.Maximize();
+            driver.Navigate().Refresh();
+
+            var vxod = (new WebDriverWait(driver, timeout)).Until(ExpectedConditions.ElementIsVisible(By.XPath("/html/body/div[1]/header/nav/div/div/ul/li[2]/a")));
+            vxod.Click();
+
+            var mail = (new WebDriverWait(driver, timeout)).Until(ExpectedConditions.ElementIsVisible(By.XPath("/html/body/div[5]/div[1]/div[2]/div[2]/div[1]/div/form/div/div[2]/div[2]/input")));
+            mail.SendKeys("stepan.mitrofanov.2015@mail.ru");
+            var password = driver.FindElement(By.XPath("/html/body/div[5]/div[1]/div[2]/div[2]/div[1]/div/form/div/div[2]/div[3]/input"));
+            password.SendKeys("123456");
+            var sub = driver.FindElement(By.XPath("//*[@id=\"js_auth_button\"]"));
+            sub.Click();
+            text = (new WebDriverWait(driver, timeout)).Until(ExpectedConditions.ElementIsVisible(By.XPath("/html/body/div[3]/header/div/div/div[4]/div/div/table/tbody/tr/td[1]/span"))).Text;
+
+        }
+    }
+}
--- UnitTestAvto/UnitTest1.cs
+++ UnitTestAvto/UnitTest1.cs
@@ -1,14 +1,48 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
 
-namespace UnitTestAvto
+namespace TeacoSubscribeNunit
 {
-    [TestClass]
+    [TestFixture]
     public class UnitTest1
     {
-        [TestMethod]
+        IWebDriver driver;
+
+        //[OneTimeSetUp] // вызывается перед началом запуска всех тестов
+        //public void OneTimeSetUp()
+        //{
+         
+        //}
+
+        //[OneTimeTearDown] //вызывается после завершения всех тестов
+        //public void OneTimeTearDown()
+        //{
+
+        //}
+
+        [SetUp] // вызывается перед каждым тестом
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [TearDown] // вызывается после каждого теста
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
+        [Test]
         public void TestMethod1()
         {
+            var actual = "Пользователь";
+
+            Avtorizacia avto = new Avtorizacia(driver);
+                        avto.Test();
+
+            Assert.AreEqual(avto.text, actual);
         }
     }
 }
+
